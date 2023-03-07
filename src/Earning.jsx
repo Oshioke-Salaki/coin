47d88557b54e1d13921e296a9ac9e4c8e8cd5f62
Oshioke-Salaki <salaki1902@gmail.com>
2023-03-06 22:16:06 -0600

    updated

--- src/Earning.jsx
+++ src/Earning.jsx
@@ -1,6 +1,11 @@
 import React from 'react';
 import './Earning.css';
 
+import near from './assets/near.png';
+import shp from './assets/shp.png';
+import amp from './assets/amp.png';
+import graph from './assets/graph.png';
+
 export default function Earning() {
   return (
     <div className="earning">
@@ -13,6 +18,34 @@ export default function Earning() {
         <br /> out for yourself.
       </p>
       <a href="#">Start Earning</a>
+      <div className="earn">
+        <div className="earnLeft">
+          <img src={graph} alt="" />
+          <h2>The Graph</h2>
+        </div>
+        <h3>Earn $4 GRT</h3>
+      </div>
+      <div className="earn">
+        <div className="earnLeft">
+          <img src={amp} alt="" />
+          <h2>AMP</h2>
+        </div>
+        <h3>Earn $3 AMP</h3>
+      </div>
+      <div className="earn">
+        <div className="earnLeft">
+          <img src={shp} alt="" />
+          <h2>SHPING</h2>
+        </div>
+        <h3>Earn $14 SHPING</h3>
+      </div>
+      <div className="earn">
+        <div className="earnLeft">
+          <img src={near} alt="" />
+          <h2>NEAR Protocol</h2>
+        </div>
+        <h3>Earn $3 NEAR</h3>
+      </div>
     </div>
   );
 }

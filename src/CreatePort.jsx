45ce06d4918f88e8946df67c2be6610e9d80b88c
Oshioke-Salaki <salaki1902@gmail.com>
2023-03-20 21:37:22 -0600

    updated

--- src/CreatePort.jsx
+++ src/CreatePort.jsx
@@ -17,7 +17,66 @@ export default function CreatePort() {
       <img src={ill1} alt="" className="illus" />
       <div className="property">
         <div className="propImg">
-          <img src="" alt="" />
+          <svg
+            width="30px"
+            height="32px"
+            viewBox="0 0 30 32"
+            xmlns="http://www.w3.org/2000/svg"
+            xmlns:xlink="http://www.w3.org/1999/xlink"
+          >
+            <desc>Created with Sketch.</desc>
+            <defs></defs>
+            <g
+              id="Second-Group"
+              stroke="none"
+              stroke-width="1"
+              fill="none"
+              fill-rule="evenodd"
+            >
+              <g
+                id="V2---All-new"
+                transform="translate(-683.000000, -1591.000000)"
+              >
+                <g id="Group-20" transform="translate(0.000000, 1068.000000)">
+                  <g
+                    id="Group-48"
+                    transform="translate(662.000000, 229.000000)"
+                  >
+                    <g
+                      id="Group-10"
+                      transform="translate(0.000000, 270.000000)"
+                    >
+                      <g
+                        id="Group-38"
+                        transform="translate(0.000000, 4.000000)"
+                      >
+                        <g
+                          id="Group"
+                          transform="translate(21.000000, 20.000000)"
+                        >
+                          <polygon
+                            id="Fill-1"
+                            fill="#D4EEFF"
+                            points="10 32 20 32 20 10 10 10"
+                          ></polygon>
+                          <polygon
+                            id="Fill-2"
+                            fill="#1752F0"
+                            points="20 32 30.0005 32 30.0005 0 20 0"
+                          ></polygon>
+                          <polygon
+                            id="Fill-3"
+                            fill="#55B4FC"
+                            points="0 32 10 32 10 19 0 19"
+                          ></polygon>
+                        </g>
+                      </g>
+                    </g>
+                  </g>
+                </g>
+              </g>
+            </g>
+          </svg>
         </div>
         <h3>Manage your portfolio</h3>
         <h2>
@@ -27,7 +86,117 @@ export default function CreatePort() {
       </div>
       <div className="property">
         <div className="propImg">
-          <img src="" alt="" />
+          <svg
+            width="36px"
+            height="38px"
+            viewBox="0 0 36 38"
+            xmlns="http://www.w3.org/2000/svg"
+            xmlns:xlink="http://www.w3.org/1999/xlink"
+          >
+            <desc>Created with Sketch.</desc>
+            <defs>
+              <polygon
+                id="path-1"
+                points="0 38 35.625 38 35.625 0 0 0"
+              ></polygon>
+            </defs>
+            <g
+              id="Second-Group"
+              stroke="none"
+              stroke-width="1"
+              fill="none"
+              fill-rule="evenodd"
+            >
+              <g
+                id="V2---All-new"
+                transform="translate(-680.000000, -1837.000000)"
+              >
+                <g id="Group-20" transform="translate(0.000000, 1068.000000)">
+                  <g
+                    id="Group-48"
+                    transform="translate(662.000000, 229.000000)"
+                  >
+                    <g
+                      id="Group-40"
+                      transform="translate(0.000000, 522.000000)"
+                    >
+                      <g
+                        id="Group-18"
+                        transform="translate(0.000000, 4.000000)"
+                      >
+                        <g
+                          id="Group-42"
+                          transform="translate(18.000000, 14.000000)"
+                        >
+                          <path
+                            d="M17.8125,9.5 C19.1240938,9.5 20.1875,8.43659375 20.1875,7.125 L20.1875,2.375 C20.1875,1.06340625 19.1240938,0 17.8125,0 C16.5009062,0 15.4375,1.06340625 15.4375,2.375 L15.4375,7.125 C15.4375,8.43659375 16.5009062,9.5 17.8125,9.5"
+                            id="Fill-1"
+                            fill="#D4EEFF"
+                          ></path>
+                          <path
+                            d="M29.6875,9.5 C30.9990938,9.5 32.0625,8.43659375 32.0625,7.125 L32.0625,2.375 C32.0625,1.06340625 30.9990938,0 29.6875,0 C28.3759062,0 27.3125,1.06340625 27.3125,2.375 L27.3125,7.125 C27.3125,8.43659375 28.3759062,9.5 29.6875,9.5"
+                            id="Fill-3"
+                            fill="#D4EEFF"
+                          ></path>
+                          <g id="Clip-6"></g>
+                          <polygon
+                            id="Fill-5"
+                            fill="#D4EEFF"
+                            points="0 38 35.625 38 35.625 4.75 0 4.75"
+                          ></polygon>
+                          <polygon
+                            id="Fill-7"
+                            fill="#55B4FC"
+                            points="0 11.875 35.625 11.875 35.625 4.75 0 4.75"
+                          ></polygon>
+                          <path
+                            d="M5.9375,9.5 C7.24909375,9.5 8.3125,8.43659375 8.3125,7.125 L8.3125,2.375 C8.3125,1.06340625 7.24909375,0 5.9375,0 C4.62590625,0 3.5625,1.06340625 3.5625,2.375 L3.5625,7.125 C3.5625,8.43659375 4.62590625,9.5 5.9375,9.5"
+                            id="Fill-8"
+                            fill="#D4EEFF"
+                          ></path>
+                          <path
+                            d="M27.3125,4.75 L27.3125,7.125 C27.3125,8.43659375 28.3759062,9.5 29.6875,9.5 C30.9990938,9.5 32.0625,8.43659375 32.0625,7.125 L32.0625,4.75 L27.3125,4.75 Z"
+                            id="Fill-9"
+                            fill="#1752F0"
+                          ></path>
+                          <path
+                            d="M5.9375,9.5 C7.24909375,9.5 8.3125,8.43659375 8.3125,7.125 L8.3125,4.75 L3.5625,4.75 L3.5625,7.125 C3.5625,8.43659375 4.62590625,9.5 5.9375,9.5"
+                            id="Fill-10"
+                            fill="#1752F0"
+                          ></path>
+                          <path
+                            d="M17.8125,9.5 C19.1240938,9.5 20.1875,8.43659375 20.1875,7.125 L20.1875,2.375 C20.1875,1.06340625 19.1240938,0 17.8125,0 C16.5009062,0 15.4375,1.06340625 15.4375,2.375 L15.4375,7.125 C15.4375,8.43659375 16.5009062,9.5 17.8125,9.5"
+                            id="Fill-11"
+                            fill="#D4EEFF"
+                          ></path>
+                          <path
+                            d="M17.8125,9.5 C19.1240938,9.5 20.1875,8.43659375 20.1875,7.125 L20.1875,4.75 L15.4375,4.75 L15.4375,7.125 C15.4375,8.43659375 16.5009062,9.5 17.8125,9.5"
+                            id="Fill-12"
+                            fill="#1752F0"
+                          ></path>
+                          <polygon
+                            id="Fill-13"
+                            fill="#FFFFFF"
+                            points="4.75 34.4375 30.875 34.4375 30.875 11.875 4.75 11.875"
+                          ></polygon>
+                          <polygon
+                            id="Fill-14"
+                            fill="#1752F0"
+                            points="12.6899813 26.3287156 16.048825 29.6875594 26.1247625 19.6110281 22.7665125 16.2521844"
+                          ></polygon>
+                          <polygon
+                            id="Fill-15"
+                            fill="#1752F0"
+                            points="16.0487063 29.6875 19.40755 26.3286562 13.1642688 20.085375 9.805425 23.4442187"
+                          ></polygon>
+                        </g>
+                      </g>
+                    </g>
+                  </g>
+                </g>
+              </g>
+            </g>
+          </svg>
         </div>
         <h3>Recurring buys</h3>
         <h2>
@@ -37,7 +206,31 @@ export default function CreatePort() {
       </div>
       <div className="property">
         <div className="propImg">
-          <img src="" alt="" />
+          <svg
+            width="30"
+            height="33"
+            viewBox="0 0 30 33"
+            fill="none"
+            xmlns="http://www.w3.org/2000/svg"
+          >
+            <path
+              d="M14.9994 0C9.10658 0 4.28516 4.64062 4.28516 11.3438V25.7812H25.7137V11.3438C25.7137 4.64062 20.8923 0 14.9994 0Z"
+              fill="#D4EEFF"
+            ></path>
+            <path d="M30 12.375H0V33H30V12.375Z" fill="#56B4FC"></path>
+            <path
+              d="M25.7137 12.375H4.28516V28.875H25.7137V12.375Z"
+              fill="#1652F0"
+            ></path>
+            <path
+              d="M14.9999 18.5625L17.8392 24.75H12.1606L14.9999 18.5625Z"
+              fill="white"
+            ></path>
+            <path
+              d="M15.0003 20.625C16.1837 20.625 17.1431 19.7016 17.1431 18.5625C17.1431 17.4234 16.1837 16.5 15.0003 16.5C13.8168 16.5 12.8574 17.4234 12.8574 18.5625C12.8574 19.7016 13.8168 20.625 15.0003 20.625Z"
+              fill="white"
+            ></path>
+          </svg>
         </div>
         <h3>Vault protection</h3>
         <h2>
@@ -47,10 +240,50 @@ export default function CreatePort() {
       </div>
       <div className="property">
         <div className="propImg">
-          <img src="" alt="" />
+          <svg
+            width="36"
+            height="36"
+            viewBox="0 0 36 36"
+            fill="none"
+            xmlns="http://www.w3.org/2000/svg"
+          >
+            <g>
+              <path
+                d="M36.0005 2.25006H0.000488281V33.7501H36.0005V2.25006Z"
+                fill="#D4EEFF"
+              ></path>
+              <path
+                d="M28.1245 36.0001H7.87451C7.25576 36.0001 6.74951 35.4938 6.74951 34.8751V1.12506C6.74951 0.506311 7.25576 6.10352e-05 7.87451 6.10352e-05H28.1245C28.7433 6.10352e-05 29.2495 0.506311 29.2495 1.12506V34.8751C29.2495 35.4938 28.7433 36.0001 28.1245 36.0001Z"
+                fill="#56B4FC"
+              ></path>
+              <path
+                d="M29.2505 2.25006H6.75049V33.7501H29.2505V2.25006Z"
+                fill="#1652F0"
+              ></path>
+              <path
+                d="M22.9998 15.1371L21.8887 14.0001L15.2217 20.8224L16.3329 21.9595L22.9998 15.1371Z"
+                fill="white"
+              ></path>
+              <path
+                d="M16.3337 22L17.4448 20.8629L14.1113 17.4517L13.0002 18.5888L16.3337 22Z"
+                fill="white"
+              ></path>
+              <path
+                d="M23.6255 3.37506H12.3755C11.7567 3.37506 11.2505 2.86881 11.2505 2.25006V1.12506H24.7505V2.25006C24.7505 2.86881 24.2442 3.37506 23.6255 3.37506Z"
+                fill="#56B4FC"
+              ></path>
+              <path
+                d="M15.7505 32.0626C15.7505 32.4001 15.9755 32.6251 16.313 32.6251H19.688C20.0255 32.6251 20.2505 32.4001 20.2505 32.0626C20.2505 31.7251 20.0255 31.5001 19.688 31.5001H16.313C15.9755 31.5001 15.7505 31.7251 15.7505 32.0626Z"
+                fill="white"
+              ></path>
+            </g>
+          </svg>
         </div>
         <h3>Mobile apps</h3>
-        <h2>Stay on top of the markets with the Coinbase app for</h2>
+        <h2>
+          Stay on top of the markets with the Coinbase app for{' '}
+          <span>Android</span> or <span>IOS</span>
+        </h2>
       </div>
     </div>
   );
--- src/Trusted.jsx
+++ src/Trusted.jsx
@@ -11,6 +11,25 @@ export default function Trusted() {
       </h1>
       <h2>Here are a few reasons why you should choose Coinbase</h2>
       <div className="secure">
+        <svg
+          width="64"
+          height="64"
+          viewBox="0 0 64 64"
+          fill="none"
+          xmlns="http://www.w3.org/2000/svg"
+        >
+          <path d="M56 0H8V50H56V0Z" fill="#BFE9FF"></path>
+          <path d="M64 10H0V58H64V10Z" fill="#56B4FC"></path>
+          <path d="M56 10H8V50H56V10Z" fill="#1652F0"></path>
+          <path d="M10 16H6V24H10V16Z" fill="#1652F0"></path>
+          <path d="M10 36H6V44H10V36Z" fill="#1652F0"></path>
+          <path d="M64 58H56V64H64V58Z" fill="#56B4FC"></path>
+          <path d="M8 58H0V64H8V58Z" fill="#56B4FC"></path>
+          <path
+            d="M41.1905 20.81C36.1205 15.74 27.8705 15.74 22.8105 20.81C17.7505 25.88 17.7405 34.13 22.8105 39.19C25.3405 41.72 28.6705 42.99 32.0005 42.99C35.3305 42.99 38.6605 41.72 41.1905 39.19C46.2605 34.12 46.2605 25.88 41.1905 20.81ZM32.0005 19C34.5005 19 37.0005 19.85 39.0405 21.55L36.1905 24.4C34.9805 23.5 33.5305 23 32.0005 23C30.4705 23 29.0205 23.5 27.8105 24.4L24.9605 21.55C27.0005 19.86 29.5005 19 32.0005 19ZM35.5405 33.54C34.5905 34.48 33.3405 35 32.0005 35C30.6605 35 29.4105 34.48 28.4605 33.54C27.5105 32.6 27.0005 31.33 27.0005 30C27.0005 28.67 27.5205 27.41 28.4605 26.46C29.4005 25.51 30.6605 25 32.0005 25C33.3405 25 34.5905 25.52 35.5405 26.46C36.4905 27.4 37.0005 28.67 37.0005 30C37.0005 31.33 36.4805 32.59 35.5405 33.54ZM23.5505 22.96L26.4005 25.81C25.5005 27.02 25.0005 28.47 25.0005 30C25.0005 31.53 25.5005 32.98 26.4005 34.19L23.5505 37.04C20.1605 32.97 20.1605 27.03 23.5505 22.96ZM24.9605 38.45L27.8105 35.6C29.0105 36.5 30.4705 37 32.0005 37C33.5305 37 34.9805 36.5 36.1905 35.6L39.0405 38.45C34.9705 41.84 29.0305 41.84 24.9605 38.45ZM40.4505 37.04L37.6005 34.19C38.5005 32.99 39.0005 31.54 39.0005 30C39.0005 28.47 38.5005 27.02 37.6005 25.81L40.4505 22.96C43.8405 27.03 43.8405 32.97 40.4505 37.04Z"
+            fill="white"
+          ></path>
+        </svg>
         <h3>Secure Storage</h3>
         <h4>
           We store the vast majority of the digital
@@ -21,6 +40,34 @@ export default function Trusted() {
         </a>
       </div>
       <div className="secure">
+        <svg
+          width="64"
+          height="64"
+          viewBox="0 0 64 64"
+          fill="none"
+          xmlns="http://www.w3.org/2000/svg"
+        >
+          <path
+            d="M56 56H48V64H56C60.42 64 64 60.42 64 56H56Z"
+            fill="#BFE9FF"
+          ></path>
+          <path
+            d="M48 56V0H0V56C0 60.42 3.58 64 8 64H48H56C51.58 64 48 60.42 48 56Z"
+            fill="#56B4FC"
+          ></path>
+          <path
+            d="M48 56H0C0 60.42 3.58 64 8 64H56C51.58 64 48 60.42 48 56Z"
+            fill="#1652F0"
+          ></path>
+          <path d="M40 30H8V32H40V30Z" fill="white"></path>
+          <path d="M40 36H8V38H40V36Z" fill="white"></path>
+          <path d="M40 42H8V44H40V42Z" fill="white"></path>
+          <path d="M32 48H16V50H32V48Z" fill="white"></path>
+          <path
+            d="M23.4998 24.66L20.2598 18.74L14.3398 15.5L20.2598 12.26L23.4998 6.34L26.7398 12.26L32.6598 15.5L26.7398 18.74L23.4998 24.66ZM18.5098 15.5L21.7298 17.26L23.4898 20.48L25.2498 17.26L28.4698 15.5L25.2498 13.74L23.4898 10.52L21.7298 13.74L18.5098 15.5Z"
+            fill="white"
+          ></path>
+        </svg>
         <h3>Protected by Insurance</h3>
         <h4>
           Coinbase maintains crypto insurance and all USD cash balances are
@@ -31,6 +78,24 @@ export default function Trusted() {
         </a>
       </div>
       <div className="secure">
+        <svg
+          width="56"
+          height="64"
+          viewBox="0 0 56 64"
+          fill="none"
+          xmlns="http://www.w3.org/2000/svg"
+        >
+          <path d="M56 0H24V64H56V0Z" fill="#BFE9FF"></path>
+          <path
+            d="M0 8V36C0 49.25 24 60 24 60C24 60 48 49.25 48 36V8H0Z"
+            fill="#56B4FC"
+          ></path>
+          <path d="M24 8V60C24 60 48 49.25 48 36V8H24Z" fill="#1652F0"></path>
+          <path
+            d="M7 15V36C7 40.92 15.57 47.72 23.53 51.97L24 52.22L24.47 51.97C32.43 47.72 41 40.92 41 36V15H7ZM39 17V23H9V17H39ZM24 49.95C15.55 45.35 9 39.28 9 36V25H39V36C39 39.28 32.45 45.35 24 49.95Z"
+            fill="white"
+          ></path>
+        </svg>
         <h3>Industry best practices</h3>
         <h4>
           Coinbase supports a variety of the most popular digital currencies.
